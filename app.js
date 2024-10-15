532ae12ce3d3dad56f20f8b71c035e5652e3f9aa
Ehsan <ehsans.contractor@elementsgs.com>
2024-10-15 10:58:26 +0500

    update app.js

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ import mongoose from "mongoose";
 import feedRouter from "./routes/feed.js";
 import bodyParser from "body-parser";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(bodyParser.json());
 app.use("/", setHeaders); // to allow CORS
 
